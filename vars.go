f35240169f470fbe56612931f8831e4764a5369d
anand <you@example.com>
2020-05-04 17:27:31 +0530

    first

---
+++ vars.go
@@ -0,0 +1,14 @@
+package main
+import (
+	"fmt"
+)
+func main(){
+	var i int
+	i = 42
+	var j int = 89
+	k := 33
+	is_occupied := false
+	fmt.Println(i,"sldkf ",j,k)
+	fmt.Printf("%v, %T\n",k,k)
+	fmt.Printf("is occupied %v",is_occupied)
+}
